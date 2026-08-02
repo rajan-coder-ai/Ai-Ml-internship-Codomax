8c5bda46941c21ab11fb78fb3588d131cdc6851e
rajan-coder-ai <rajanbharti25cs@rnsit.ac.in>
2026-08-03 02:19:09 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,25 @@
+# Day 2 - Python Basics
+
+name = "Rajan"
+age = 18
+college = "RNSIT"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+
+a = 10
+b = 5
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+
+for i in range(1, 6):
+    print("Number:", i)
+
+def greet():
+    print("Welcome to AI & ML Internship")
+
+greet()
